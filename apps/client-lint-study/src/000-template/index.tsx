6035298abc9fea534142551e12fbd238d6b904c1
Apollo Tang <tang.apollo@gmail.com>
2022-03-30 13:16:23 -0400

    mv template index 001->000

--- apps/client-lint-study/src/000-template/index.tsx
+++ apps/client-lint-study/src/000-template/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import img from './react.png';
 import s from './style.module.less';
 
-/* eslint-disable-next-line */
 console.log('asfd');
 
-// @ts-expect-error example usage @ts-expeceted-error
-const foo:string = 1
+const foo: string = 1;
 
 const App = () => (
   <div className={`${s.app}`}>
