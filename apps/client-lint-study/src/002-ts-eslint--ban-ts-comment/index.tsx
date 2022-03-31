1910380859078f48aafd1c65e406f4d02c3f2a16
Apollo Tang <tang.apollo@gmail.com>
2022-03-31 03:03:07 -0400

    improve demo 003 ban-ts-comment improve comment

--- apps/client-lint-study/src/002-ts-eslint--ban-ts-comment/index.tsx
+++ apps/client-lint-study/src/002-ts-eslint--ban-ts-comment/index.tsx
@@ -9,7 +9,7 @@ foo(10000);
 // instead please use
 //   "@ts-expect-error"
 // -----------------------------------------------------
-// @ts-expect-error Reson: demo purpose
+// @ts-expect-error | Reason: demo purpose
 foo(20000);
 
 export { foo };
