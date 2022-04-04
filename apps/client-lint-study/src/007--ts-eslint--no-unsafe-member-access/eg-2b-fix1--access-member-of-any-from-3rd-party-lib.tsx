a915465fb891cf7625e567227b0c0ad5d7e8c046
Apollo Tang <tang.apollo@gmail.com>
2022-04-04 01:50:12 -0400

    fix error

--- apps/client-lint-study/src/007--ts-eslint--no-unsafe-member-access/eg-2b-fix1--access-member-of-any-from-3rd-party-lib.tsx
+++ apps/client-lint-study/src/007--ts-eslint--no-unsafe-member-access/eg-2b-fix1--access-member-of-any-from-3rd-party-lib.tsx
@@ -3,7 +3,7 @@ import { someObj1 } from '@myscope/util--fake-3rd-party-lib';
 // tsc don't know the type of data1, so it label it as 'any' type
 
 // I know the type of data and I am going assert it for you
-type someObjType = {
+type someObj1Type = {
   prop: string;
 };
-console.warn((someObj1 as someObjType).prop);
+console.warn((someObj1 as someObj1Type).prop);
--- utils/fake-3rd-party-lib/src/main.d.ts
+++ utils/fake-3rd-party-lib/src/main.d.ts
@@ -8,5 +8,5 @@ interface someObj2 {
 
 declare const data1: someObj1;
 declare const data2: someObj2;
-export { data1Typo, data2Typo /* , someObj, someObj2 */ };
+export { data1Typo, data2Typo /* , someObj1, someObj2 */ };
 //# sourceMappingURL=main.d.ts.map
