dba3598b14e8292fb3eb46e2a71879feba26dd22
Apollo Tang <tang.apollo@gmail.com>
2022-04-21 02:01:38 -0400

    add note

--- apps/client-lint-study/src/008--ts-eslint--no-explicit-any/index.tsx
+++ apps/client-lint-study/src/008--ts-eslint--no-explicit-any/index.tsx
@@ -5,14 +5,16 @@ interface MyInterface {
 }
 
 // -----------------------------
-// Avoid using @ts-expect-error:
-// But explicite mark it as any
+// In this case don't use @ts-expect-error:
+// But explicite mark it as 'any'
 // We want to show the warning in CLI for future tech-debt effort
 
 // @ts-expect-error | Demo: formal-parameter is implicit 'any'
 const myFunction_avoid = (input): void => {
   console.info(input);
 };
+
+// Do this: explicitly mark it as any
 const myFunction_ok = (input: any): void => {
   console.info(input);
 };
