707d8d03cbf3323ffe6eed8e59406a13e18d841d
Apollo Tang <tang.apollo@gmail.com>
2022-04-29 00:25:51 -0400

    update comment

--- apps/client-lint-study/src/009--ts-eslint--no-unsafe-assignment/eg-1a-error--assign-any-type.tsx
+++ apps/client-lint-study/src/009--ts-eslint--no-unsafe-assignment/eg-1a-error--assign-any-type.tsx
@@ -1,10 +1,10 @@
 import { data } from './really-bad-typescript';
 
-// warning! you are assigning an 'any' type to a variable
 const foo = data;
-
-// note typescript is okay with any (this eslint rule give you extra protection)
-// eslint-disable-next-line
-// const iTakeAny = (a:any) => a;
+//    ^^^^^^^^^^ Unsafe assignment of an `any` value.
+//
+// Note that the above is an eslint error, it is not a typescript
+// compiler error. Typescript compiler does not stop you assigning
+// an 'any' type to a variable.
 
 export { foo };
