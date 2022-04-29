c5522434b3917a30e3cc152beec30652f8f91341
Apollo Tang <tang.apollo@gmail.com>
2022-04-29 01:51:40 -0400

    make the export a typo in declaration file

--- utils/fake-3rd-party-lib/src/main.d.ts
+++ utils/fake-3rd-party-lib/src/main.d.ts
@@ -8,5 +8,6 @@ declare const someObj1: {
 
 declare const data1: someObj1;
 declare const data2: someObj2;
-export { data1Typo, data2Typo /* , someObj1, someObj2 */ };
-//# sourceMappingURL=main.d.ts.map
+
+export { data1Typo, data2Typo };
+export { someObj1Typo, someObj2Typo };
