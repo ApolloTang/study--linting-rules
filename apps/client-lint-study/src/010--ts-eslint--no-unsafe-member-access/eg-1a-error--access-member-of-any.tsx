1f29ec894bfbbf411372a686d30cede8b86a89f4
Apollo Tang <tang.apollo@gmail.com>
2022-04-29 02:05:41 -0400

    improve comment

--- apps/client-lint-study/src/010--ts-eslint--no-unsafe-member-access/eg-1a-error--access-member-of-any.tsx
+++ apps/client-lint-study/src/010--ts-eslint--no-unsafe-member-access/eg-1a-error--access-member-of-any.tsx
@@ -1,4 +1,6 @@
 import { untypedObj } from './really-bad-typescript';
 
-// warning! you are accessing a property on an 'any' type object
 console.warn(untypedObj.prop);
+//           ^^^^^^^^^^^^^^^
+//           Unsafe member access .prop on an `any` value.
+//           warning! you are accessing a property on an 'any' type object
