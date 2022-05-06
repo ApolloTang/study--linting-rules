555847b2ca17bcaf4722ca279082a24fddc77da3
Apollo Tang <tang.apollo@gmail.com>
2022-05-06 00:16:25 -0400

    016 update no ban type

--- apps/client-lint-study/src/016--ts-eslint--ban-types--wip/eg-typing-string-w-ban-type.ts
+++ apps/client-lint-study/src/016--ts-eslint--ban-types--wip/eg-typing-string-w-ban-type.ts
@@ -1,6 +1,20 @@
-const foo = String('foo');
+console.info(String('foo') === 'foo'); // true
+console.info(new String('foo') === 'foo'); // false
 
 // eslint-disable-next-line
-const bar = 'bar' as unknown as String;
+function isFoo(input: String) {
+  return input === 'foo';
+}
+isFoo('foo'); // true
+isFoo(String('foo')); // true
+isFoo(new String('foo')); // false
 
-export { foo, bar };
+function isFoo2(input: string) {
+  return input === 'foo';
+}
+isFoo2('foo'); // true
+isFoo2(String('foo')); // true
+// @ts-expect-error Argument of type 'String' is not assignable to parameter of type 'string'
+isFoo2(new String('foo')); // false
+
+export {};
